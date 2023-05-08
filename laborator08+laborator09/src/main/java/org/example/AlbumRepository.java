8b79e2a2e98288f898bd8f8fddd74901fce24de5
valigrigore27 <126250159+valigrigore27@users.noreply.github.com>
2023-05-08 20:00:09 +0300

    Update AlbumRepository.java

--- laborator08+laborator09/src/main/java/org/example/AlbumRepository.java
+++ laborator08+laborator09/src/main/java/org/example/AlbumRepository.java
@@ -1,10 +1,14 @@
 package org.example;
 
 import javax.persistence.EntityManager;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import java.util.List;
 
 public class AlbumRepository {
     private EntityManager em;
+    @OneToOne
+    private List<Artist> artists;
 
     public AlbumRepository(EntityManager em) {
         this.em = em;
@@ -36,3 +40,19 @@ public class AlbumRepository {
                 .getResultList();
     }
 }
+//package org.example;
+//
+//        import javax.persistence.EntityManager;
+//        import java.util.List;
+//
+//public class AlbumRepository extends AbstractRepository<Album> {
+//    public AlbumRepository(EntityManager em) {
+//        super(em);
+//    }
+//
+//    public List<Album> findByArtist(Artist artist) {
+//        return getEm().createNamedQuery("Album.findByArtist")
+//                .setParameter("artist", artist)
+//                .getResultList();
+//    }
+//}
